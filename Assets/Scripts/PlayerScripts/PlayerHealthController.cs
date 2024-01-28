869a03015ea0622c2ee63cd5c29ac185aa478e2c
EgorF <egor1268@gmail.com>
2024-01-28 16:32:13 +0500

    implemented health pickup

--- Assets/Scripts/PlayerScripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealthController.cs
@@ -5,10 +5,17 @@ using UnityEngine.Events;
 
 public class PlayerHealthController : MonoBehaviour
 {
-    public UnityEvent OnDamageTaken;
+    public UnityEvent OnHealthChanged;
     public UnityEvent OnDeath;
 
-    [SerializeField] private int _health = 3;
+    [SerializeField] private int _maxHealth = 3;
+
+    private int _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,19 +27,31 @@ public class PlayerHealthController : MonoBehaviour
 
     public void TakeDamage(int amount)
     {
-        _health -= amount;
-        OnDamageTaken.Invoke();
+        _currentHealth -= amount;
+        OnHealthChanged.Invoke();
 
-        if (_health <= 0 )
+        if (_currentHealth <= 0 )
         {
             OnDeath.Invoke();
             Destroy(gameObject);
         }
     }
 
+    public void AddHealth(int amount)
+    {
+        _currentHealth += amount;
+
+        if (_currentHealth > _maxHealth)
+        {
+            _currentHealth = _maxHealth;
+        }
+
+        OnHealthChanged.Invoke();
+    }
+
     public int GetHealth()
     { 
-        return _health; 
+        return _currentHealth; 
     }
 
 }
