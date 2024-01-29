c40c9b40e71db00f14d7ed8147ac0b6afed4674f
EgorF <egor1268@gmail.com>
2024-01-29 16:12:19 +0500

    implemented pause window

---
+++ Assets/Scripts/UI/PauseWindow.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseWindow : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void ContinueGame()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(SceneIndexes.MAIN_MENU);
+    }
+}
