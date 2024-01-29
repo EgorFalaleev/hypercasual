1bbbda23b3562582c5b68f6a10022b5547e216b6
EgorF <egor1268@gmail.com>
2024-01-29 15:59:53 +0500

    fixed start button behavior

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -7,6 +7,6 @@ public class MainMenuManager : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneIndexes.LEVEL_1);
     }
 }
