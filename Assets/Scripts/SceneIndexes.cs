8f841453117aeea17a692accc06d18c0c5bde109
EgorF <egor1268@gmail.com>
2024-01-29 15:37:30 +0500

    implemented defeat window

---
+++ Assets/Scripts/SceneIndexes.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneIndexes 
+{
+    public const int MAIN_MENU = 0;
+    public const int LEVEL_1 = 1;
+}
