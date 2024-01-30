edd97a35e9221fff94c3817729117f874a57fdb2
EgorF <egor1268@gmail.com>
2024-01-30 20:16:31 +0500

    initial firebase setup

---
+++ Assets/Scripts/Integration/FirebaseInit.cs
@@ -0,0 +1,27 @@
+using Firebase;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FirebaseInit : MonoBehaviour
+{
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                var app = FirebaseApp.DefaultInstance;
+            }
+            else
+            {
+                Debug.LogError(String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+}
