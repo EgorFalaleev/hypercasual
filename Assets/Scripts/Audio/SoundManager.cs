0361112c44ac3195ca69689892a1e86c17bbe3a0
EgorF <egor1268@gmail.com>
2024-01-31 11:48:47 +0500

    fixed sound saving bugs

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -7,8 +7,8 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance;
 
     public float Volume { get; private set; }
-    public int IsMusicOn { get; private set; }
-    public int IsEffectsOn { get; private set; }
+    public string IsMusicOn { get; private set; }
+    public string IsEffectsOn { get; private set; }
 
     [SerializeField] private AudioSource _musicSource;
     [SerializeField] private AudioSource _effectSource;
@@ -26,6 +26,16 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        IsMusicOn = SaveManager.Instance.IsMusicOn;
+        IsEffectsOn = SaveManager.Instance.IsEffectsOn;
+        Volume = SaveManager.Instance.MasterVolume;
+        _musicSource.mute = SaveManager.Instance.IsMusicOn == "On" ? false : true;
+        _effectSource.mute = SaveManager.Instance.IsEffectsOn == "On" ? false : true;
+        ChangeMasterVolume(SaveManager.Instance.MasterVolume);
+    }
+
     public void PlayEffectSound(AudioClip clip)
     {
         _effectSource.PlayOneShot(clip);
@@ -34,20 +44,21 @@ public class SoundManager : MonoBehaviour
     public void ChangeMasterVolume(float value)
     {
         Volume = value;
-        AudioListener.volume = value;
+        _musicSource.volume = value;
+        _effectSource.volume = value;
     }
 
     public void ToggleEffects()
     {
         _effectSource.mute = !_effectSource.mute;
 
-        IsEffectsOn = _effectSource.mute ? 0 : 1;
+        IsEffectsOn = _effectSource.mute ? "Off" : "On";
     }
 
     public void ToggleMusic()
     {
         _musicSource.mute = !_musicSource.mute;
 
-        IsMusicOn = _musicSource.mute ? 0 : 1;
+        IsMusicOn = _musicSource.mute ? "Off" : "On";
     }
 }
